057e7d19daad1ef4af8d5f04f3d6c79f4bf25c75
leonardomagalhaes21 <leonardomagalhaess21@gmail.com>
2024-04-13 03:45:16 +0100

    fixing conflits

--- app_code/lib/message_screen.dart
+++ app_code/lib/message_screen.dart
@@ -178,15 +178,35 @@ class _MessageScreenState extends State<MessageScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Chat'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.search),
-            onPressed: () {
-              // Implement search functionality
-            },
-          ),
-        ],
+        automaticallyImplyLeading: false,
+        backgroundColor: const Color.fromRGBO(240, 240, 240, 1),
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  bottom: 4.0,
+                ),
+                child: Text(
+                  'FEUP-reUSE',
+                  style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontSize: 39.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              height: 4,
+              color: Colors.black,
+            ),
+          ],
+        ),
+        centerTitle: true,
+        elevation: 4,
       ),
       body: Column(
         children: [
