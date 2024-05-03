c005e3207512904ce48b29136d078d0d01a4f52b
Terooo <up202204971@up.pt>
2024-05-03 23:54:30 +0100

    tests

--- app_code/test/register_screen_test.dart
+++ app_code/test/register_screen_test.dart
@@ -2,20 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:mockito/mockito.dart';
-import '../lib/register_screen.dart'; // Update with the correct path
+import '../lib/register_screen.dart'; 
 
-// Mock FirebaseAuth for testing
 class MockFirebaseAuth extends Mock implements FirebaseAuth {}
 
 void main() {
   testWidgets('RegisterScreen widgets test', (WidgetTester tester) async {
-    // Build our widget and trigger a frame.
     await tester.pumpWidget(MaterialApp(home: RegisterScreen()));
 
-    // Verify that the RegisterScreen has text fields for name, email, password, and confirm password.
     expect(find.byType(TextField), findsNWidgets(4));
 
-    // Verify that the RegisterScreen has an ElevatedButton.
     expect(find.byType(ElevatedButton), findsOneWidget);
   });
 }
