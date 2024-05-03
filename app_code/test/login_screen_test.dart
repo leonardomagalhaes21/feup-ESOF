ce009bdfcfa148587fcb909ccf79f1037cc2fc0b
Terooo <up202204971@up.pt>
2024-05-03 23:54:30 +0100

    tests

--- app_code/test/login_screen_test.dart
+++ app_code/test/login_screen_test.dart
@@ -4,20 +4,18 @@ import '../lib/login_screen.dart'; // Update with the correct path
 
 void main() {
   testWidgets('LoginScreen widgets test', (WidgetTester tester) async {
-    // Build our widget and trigger a frame.
     await tester.pumpWidget(MaterialApp(home: LoginScreen()));
 
-    // Verify that the LoginScreen title is rendered.
     expect(find.text('Login'), findsWidgets);
 
-    // Verify that the LoginScreen has text fields for email and password.
     expect(find.byType(TextField), findsNWidgets(2));
 
-    // Verify that the LoginScreen has two ElevatedButtons for login and register.
     expect(find.byType(ElevatedButton), findsNWidgets(2));
 
-    // Verify that the LoginScreen has the description text widgets.
     expect(find.text('FEUP-reUSE'), findsOneWidget);
-    expect(find.text('O FEUP-reUSE ajuda-te a partilhar e encontrar recursos reutilizáveis para um mundo mais sustentável.'), findsOneWidget);
+    expect(
+        find.text(
+            'O FEUP-reUSE ajuda-te a partilhar e encontrar recursos reutilizáveis para um mundo mais sustentável.'),
+        findsOneWidget);
   });
 }
