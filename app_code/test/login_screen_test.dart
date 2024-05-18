6395da5594680f36940eba9f4f80bf3f925b0473
Diogo Vieira <145665382+DiogoSV7@users.noreply.github.com>
2024-05-18 19:26:31 +0100

    Update login_screen_test.dart

--- app_code/test/login_screen_test.dart
+++ app_code/test/login_screen_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import '../lib/login_screen.dart'; // Update with the correct path
+import '../lib/login_screen.dart'; 
 
 void main() {
   testWidgets('LoginScreen widgets test', (WidgetTester tester) async {
@@ -15,7 +15,48 @@ void main() {
     expect(find.text('FEUP-reUSE'), findsOneWidget);
     expect(
         find.text(
-            'O FEUP-reUSE ajuda-te a partilhar e encontrar recursos reutilizáveis para um mundo mais sustentável.'),
+            'FEUP-reUSE helps you share and find reusable resources for a more sustainable world.'),
         findsOneWidget);
   });
+  testWidgets('LoginScreen email and password input test', (WidgetTester tester) async {
+  await tester.pumpWidget(MaterialApp(home: LoginScreen()));
+
+  expect(find.text(''), findsNWidgets(2));
+  await tester.enterText(find.byType(TextField).first, 'test@example.com');
+  await tester.pump();
+
+  expect(find.text('test@example.com'), findsOneWidget);
+
+  await tester.enterText(find.byType(TextField).last, 'password');
+  await tester.pump();
+
+  expect(find.text('password'), findsOneWidget);
+});
+
+testWidgets('LoginScreen login button test', (WidgetTester tester) async {
+  await tester.pumpWidget(MaterialApp(home: LoginScreen()));
+
+  expect(find.text('Login'), findsOneWidget);
+
+  await tester.tap(find.text('Login'));
+  await tester.pump();
+
+  expect(find.text('Login'), findsOneWidget);
+});
+
+testWidgets('LoginScreen register button test', (WidgetTester tester) async {
+  await tester.pumpWidget(MaterialApp(home: LoginScreen()));
+
+  await tester.tap(find.text('Register'));
+  await tester.pump();
+
+  expect(find.text('Register'), findsOneWidget);
+});
+
+testWidgets('LoginScreen Reuse Reduce Recycle text test', (WidgetTester tester) async {
+  await tester.pumpWidget(MaterialApp(home: LoginScreen()));
+
+  expect(find.text('Reuse'), findsOneWidget);
+});
+
 }
