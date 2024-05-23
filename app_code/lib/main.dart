0f8937621bf54a0f4031e2e12e2d9a86c8830de4
Terooo <up202204971@up.pt>
2024-05-23 15:40:18 +0100

    changelog, readmesprint3, titles centered and rating deleted

--- app_code/lib/main.dart
+++ app_code/lib/main.dart
@@ -89,14 +89,16 @@ class MainScreen extends StatelessWidget {
       appBar: AppBar(
         automaticallyImplyLeading: false,
         backgroundColor: const Color.fromRGBO(240, 240, 240, 1),
-        title: Text(
-          "FEUP-reUSE",
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 30,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 1.2,
-            fontFamily: GoogleFonts.oswald().fontFamily,
+        title: Center(
+          child: Text(
+            "FEUP-reUSE",
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 1.2,
+              fontFamily: GoogleFonts.oswald().fontFamily,
+            ),
           ),
         ),
       ),
